b23b016137f1bb8c674ff095dac4f055e6968e6e
pvalle <paul.valle@orbian.com>
2019-01-25 08:45:57 +0000

    adding zipkin and Rabbit MQ

--- src/main/java/uk/co/datadisk/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ src/main/java/uk/co/datadisk/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package uk.co.datadisk.microservices.currencyexchangeservice.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private Environment environment;
@@ -28,6 +32,8 @@ public class CurrencyExchangeController {
 
         exchangeValue.setPort(
                 Integer.parseInt(environment.getProperty("local.server.port")));
+
+        logger.info("{}", exchangeValue);
 
         return exchangeValue;
     }
